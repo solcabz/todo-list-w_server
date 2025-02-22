21d5654d522be499d6babb8c8ccb7ee32aa45974
Solomon cabreza <solcabreza@gmail.com>
2025-02-22 18:24:21 +0800

    serving port

--- todo-server/app.js
+++ todo-server/app.js
@@ -5,6 +5,11 @@ const dotenv =require('dotenv');
 const app  = express();
 const PORT  = process.env.PORT || 3000;
 
+
+app.get("/", (req, res) => {
+    res.send("Server Running");
+})
+
 app.listen(PORT, () => {
     console.log(`Server is Running on Port ${PORT}`);
 })
