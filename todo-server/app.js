45ec720aca575a8f6b6cc38e771bb79e0a73a888
Solomon cabreza <solcabreza@gmail.com>
2025-02-22 18:19:36 +0800

    server add env file

--- todo-server/app.js
+++ todo-server/app.js
@@ -2,5 +2,10 @@ const express = require('express');
 const dotenv =require('dotenv');
 
 
-dotenv.config();
 const app  = express();
+const PORT  = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is Running on Port ${PORT}`);
+})
+
