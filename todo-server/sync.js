d56daad1cc867b73f543c14473ab070c5d32f577
Solomon cabreza <solcabreza@gmail.com>
2025-02-24 07:49:54 +0800

    deleted sync.js

--- todo-server/sync.js
+++
@@ -1,7 +0,0 @@
-import sequelize from './config/database.js';
-import './models/User.js';
-import './models/Task.js';
-
-sequelize.sync({ alter: true }) // Creates or updates tables
-  .then(() => console.log('Database synced!'))
-  .catch(err => console.error('Error syncing database:', err));
